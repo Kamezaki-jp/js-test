723aed159f3301d1e1a35bc5094e95371c5e10a9
Kamezaki-jp <champagne.es335@gmail.com>
2021-05-12 10:23:40 +0900

    map object

---
+++ map.js
@@ -0,0 +1,17 @@
+// object作成
+const myMap = new Map();
+
+// setで値の設定
+myMap.set('id',3);
+myMap.set('name','本田');
+// 連想配列として表示
+console.log(myMap);
+// keyでvalueを取得
+console.log(myMap.get('name'));
+
+// for文との相性○
+const valueList = myMap.values();
+
+for ( value of valueList ){
+  console.log(value);
+}
